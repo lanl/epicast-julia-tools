541060a41d004a1581694b7f172d9ba00bb8b069
Joy Kitson <jkitson@lanl.gov>
2025-03-25 14:55:33 -0400

    feat: switched to saving offsets to seperate file and only save destination edges (offsets let us compute the source edge)

--- create_social_network.jl
+++ create_social_network.jl
@@ -6,43 +6,58 @@ Pkg.activate("UrbanPop")
 using UrbanPop
 using Graphs
 
-function write_header!(out_stream::IOStream,
-        graph::AbstractGraph,
-        all_pops::AbstractVector{<:Int64})
-    n_edges = Graphs.degree(graph)
-    person_edge_offsets = cumsum(n_edges) - n_edges
-
-    ne = Graphs.ne(graph)
-    nv = Graphs.nv(graph)
-    print("Generated graph with $ne edges, $nv nodes\n")
-    write(out_stream, ne)
-    write(out_stream, nv)
-    write(out_stream, person_edge_offsets)
+function get_edgelist(graph::AbstractGraph{T}) where T<:Integer
+    return reduce(vcat, [[Graphs.src(e) .% T, Graphs.dst(e) .% T]
+                         for e in Graphs.edges(graph)])
 end
 
-function get_edgelist(graph::AbstractGraph)
-    return reduce(vcat, [[Graphs.src(e), Graphs.dst(e)]
-                         for e in Graphs.edges(graph)])
+function get_edge_offsets(graph::AbstractGraph{T}
+    ) where T<:Integer
+    degrees = Graphs.degree(graph)
+    person_edge_offsets = cumsum(degrees) - degrees
+    append!(person_edge_offsets, Graphs.ne(graph) .% T)
+    return person_edge_offsets
+end
+
+function get_dsts(graph::AbstractGraph{T}) where T<:Integer
+    return [Graphs.dst(e) - 1 .% T for e in Graphs.edges(graph)]
+end
+
+function write_header!(out_stream::IOStream,
+        graph::AbstractGraph{T},
+        all_pops::AbstractVector{<:Int64}) where T<:Integer
+    g_type = summary(graph)
+    ne = Graphs.ne(graph)
+    nv = Graphs.nv(graph)
+    println("Generated $g_type with $ne edges, $nv nodes")
+    write(out_stream, ne .% T)
+    write(out_stream, nv .% T)
+
+    offsets = get_edge_offsets(graph)
+    write(out_stream, offsets .% T)
 end
 
 function main()
     in_dir = "../data"
     tracts_file = "$in_dir/49.tracts.bin"
     agents_file = "$in_dir/49.agents.bin"
-    out_file = "$in_dir/49.social.bin"
+    offsets_file = "$in_dir/49.social_offsets.bin"
+    edges_file = "$in_dir/49.social_edges.bin"
 
     all_tracts, all_pop = UrbanPop.all_tract_data(in_dir)
     total_pop = sum(all_pop)
 
-    g = Graphs.newman_watts_strogatz(total_pop, 100, 0.1)
+    g = Graphs.newman_watts_strogatz(UInt64(total_pop), 100, 0.1)
 
     # Todo: write seperate file for each state, similar to
     # how tracts and agents work
-    open(out_file, "w") do out_stream
-        write_header!(out_stream, g, all_pop)
+    open(offsets_file, "w") do offset_stream
+        write_header!(offset_stream, g, all_pop)
+    end
 
-        edges = get_edgelist(g)
-        write(out_stream, edges)
+    open(edges_file, "w") do edge_stream
+        dsts = get_dsts(g)
+        write(edge_stream, dsts)
     end
 end
 
