19c535a56e85d2aab0d9d8f529f636e657c248f4
Scottie Alexander <palexander@lanl.gov>
2025-06-25 11:28:20 -0600

    Fix typo in default ylab for geo-plot timeseries

--- EpicastGeoplot/src/EpicastGeoplot.jl
+++ EpicastGeoplot/src/EpicastGeoplot.jl
@@ -485,7 +485,7 @@ function add_state_timeseries!(ax, data::GeoplotData{T}, var::AbstractString,
     frame::Integer=1, vertical::Bool=false, start_date::AbstractString="",
     top::Integer=typemax(Int), AT::Type{<:AbstractGeo}=State;
     geo_ids::AbstractVector=[], title::AbstractString="",
-    ylab::AbstractString="New infectiond per 100k residents",
+    ylab::AbstractString="New infections per 100k residents",
     legend_kws=DEFAULT_LEGEND, ymax::Real=NaN, gap::Real=0) where T<:AbstractGeo
 
     # if data are already normalized (cases-per-100k) then simply averaging
